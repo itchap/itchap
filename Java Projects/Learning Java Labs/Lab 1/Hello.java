ed4ceb95a8dee2fae54cc7d9cd0fef5342095232
Peter Smith <itchap@gmail.com>
2024-05-10 15:13:28 +0200

    First lab
    first lab

---
+++ Java Projects/Learning Java Labs/Lab 1/Hello.java
@@ -0,0 +1,11 @@
+/* 
+** Author: Peter Smith  Date: 10 May 2024
+** Demonstrates the basic structure of a Java application
+*/
+public class Hello {
+    // prints a simple message
+    public static void main(String args[])
+    {
+        System.out.print("Hello, I'm a Java Program.\n");
+    }
+}
