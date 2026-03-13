a707c552e600721b13835c6639b9c1fbfa689224
Peter Smith <itchap@gmail.com>
2026-03-13 01:35:58 +0100

    learning
    learning

--- Apps/learning-cycle/vite.config.js
+++ Apps/learning-cycle/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/app/learning/', // <-- ADD THIS EXACT LINE
 })
