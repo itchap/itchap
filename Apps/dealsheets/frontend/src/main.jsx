160146486bbcdfeb88da46c342fa567e93337c69
Peter Smith <itchap@gmail.com>
2026-03-16 13:26:43 +0100

    test
    test

--- Apps/dealsheets/frontend/src/main.jsx
+++ Apps/dealsheets/frontend/src/main.jsx
@@ -1,6 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'; // <--- Import this
 import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/app/dealsheets"> {/* <--- Wrap it here */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
